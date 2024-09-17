5b3387469adcaf17b4a63e3988048c6fea6be3c9
ПК <@DESKTOP-5UNSHBK>
2024-09-17 20:42:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("hey King, how many estates, duchies and provinces do you have? (write in the 1, 2, 3 format");
+int ME, MD, MP;
+ME = Convert.ToInt32(Console.ReadLine());
+MD = Convert.ToInt32(Console.ReadLine());
+MP = Convert.ToInt32(Console.ReadLine());
+
+MD = MD * 3;
+MP = MP * 6;
+int TotalM = ME + MD + MP;
+
+Console.WriteLine($"You got {TotalM} points total");
